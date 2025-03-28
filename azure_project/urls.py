bbf2907c0d18f1943eec64cc4e12c188e21583cb
phanupan <81475304+phanupan@users.noreply.github.com>
2025-03-28 12:11:26 +0700

    Update urls.py

--- azure_project/urls.py
+++ azure_project/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
          views.StoreDetailUpdateDelete.as_view(), name='store_detail'),
     path('api/stores/deleteAll/', views.StoreDeleteAll.as_view(),
          name='store_delete_all'),
+    
+    path('api/products/', views.ProductList.as_view()),
+     path('api/products/<int:product_id>/',
+         views.ProductDetailUpdateDelete.as_view(), name='product_detail'),
+     path('api/products/deleteAll/', views.ProductDeleteAll.as_view(),
+         name='product_delete_all'),
 ]
