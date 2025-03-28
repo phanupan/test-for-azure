216ab3fb5b8a40fa6a01d6acfb3e4a56b8680b54
phanupan <81475304+phanupan@users.noreply.github.com>
2025-03-28 12:09:30 +0700

    Update models.py

--- api/models.py
+++ api/models.py
@@ -10,3 +10,12 @@ class Store(models.Model):
 
     def __str__(self):
         return f"{self.store_id} - {self.store_location}"
+
+class Product(models.Model):
+    product_id = models.IntegerField(unique=True)
+    product_name = models.CharField(max_length=100)
+    product_created_date = models.DateTimeField(auto_now_add=True)
+    product_updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.product_id} - {self.product_name}"
